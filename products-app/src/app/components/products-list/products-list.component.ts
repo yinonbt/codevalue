af3f9e9657b5632f9586dcad0fe7b2b8f4c1ec4f
yinon <yinon.bentarya@gmail.com>
2019-06-04 01:02:11 +0300

    start of search

--- products-app/src/app/components/products-list/products-list.component.ts
+++ products-app/src/app/components/products-list/products-list.component.ts
@@ -1,21 +1,49 @@
 import { Product } from './../../interfaces/product';
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.scss']
 })
-export class ProductsListComponent implements OnInit {
+export class ProductsListComponent implements OnInit, OnChanges {
+  
   @Input() products: Product[];
   @Input() selectedProduct: Product;
   @Output()
   productSelected = new EventEmitter<Product>();
   @Output()
   productDeleted = new EventEmitter<Product>();
-  constructor() {}
+  searchQuery = '';
+  searchTermChanged = new BehaviorSubject<string>(this.searchQuery);
+  productsAfterFilter$: Observable<Product[]>;
+  
+  constructor() {
+    this.productsAfterFilter$ = this.searchTermChanged
+      .pipe(debounceTime(200),
+        map(x => x.trim()),
+        distinctUntilChanged(),
+        map(keyword => {
+          return this.products.filter(show => show.name.indexOf(keyword) !== -1);
+        }));
+  }
 
-  ngOnInit() {}
+  ngOnChanges(changes: import("@angular/core").SimpleChanges): void {
+    this.filterProducts(this.searchQuery);
+  }
+
+  ngOnInit() {
+    this.filterProducts(this.searchQuery);
+    // this.showsAfterFilter$ = this.searchTermChanged
+    //   .pipe(debounceTime(200),
+    //     map(x => x.trim()),
+    //     distinctUntilChanged(),
+    //     map(keyword => {
+    //       return this.products.filter(show => show.name.indexOf(keyword) !== -1);
+    //     }));
+  }
 
   onProductSelected(product: Product) {
     this.selectedProduct = product;
@@ -24,5 +52,9 @@ export class ProductsListComponent implements OnInit {
 
   onProductDeleted(product: Product) {
     this.productDeleted.emit(product);
+  }
+
+  filterProducts(val: string) {
+    this.searchTermChanged.next(val);
   }
 }
